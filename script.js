45efc7626560f2b19e3b7daf91cdb76b900a4d86
Athrv0 <exposereality8@gmail.com>
2025-03-18 18:33:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,12 @@ function toggleMenu() {
   const navMenu = document.getElementById("navMenu");
   navMenu.classList.toggle("open");
 }
+
+// Close menu if clicked anywhere outside
+document.addEventListener("click", (e) => {
+  const navMenu = document.getElementById("navMenu");
+  const menuButton = document.querySelector(".menu-btn");
+  if (!navMenu.contains(e.target) && !menuButton.contains(e.target)) {
+    navMenu.classList.remove("open");
+  }
+});
